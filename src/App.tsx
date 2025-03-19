23bd7787a7a38eec8e6a3c1e141f3052acf66d34
Roman Rachkov <pakycb84@gail.com>
2025-03-19 16:18:28 +1000

    add react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import {Container} from "react-bootstrap";
 import HeaderBanner from "./Components/HeaderBaner";
 import RegistrationForm from "./pages/RegistrationForm";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {BrowserRouter, Route, Routes} from "react-router";
 import AdminMain from "./pages/admin/Main";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000, // 5 минут
+    },
+  },
+});
 
 function App() {
   return (
@@ -21,6 +29,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </Container>
+      <ReactQueryDevtools initialIsOpen={false}/>
     </QueryClientProvider>
   )
 }
