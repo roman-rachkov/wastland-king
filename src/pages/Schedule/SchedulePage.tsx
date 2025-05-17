603e87f6ea10f1a4b5fef5bf8c8c610346363b9b
Roman Rachkov <pakycb84@gail.com>
2025-05-17 10:16:06 +1000

    fix: link schedule

--- src/pages/Schedule/SchedulePage.tsx
+++ src/pages/Schedule/SchedulePage.tsx
@@ -1,10 +1,11 @@
 import {Col, Row} from "react-bootstrap";
+import {DateTime} from "luxon";
 
 const SchedulePage = () => {
   return (
     <Row className={'flex-grow-1'}>
       <Col>
-        <object data="/SCHEDULE.pdf" type="application/pdf" width="100%" height="100%">
+        <object data={`/SCHEDULE.pdf?t=${DateTime.now().toUnixInteger()}`} type="application/pdf" width="100%" height="100%">
           <p>Dont see file? <a href="/SCHEDULE.pdf">Download it</a></p>
         </object>
       </Col>
